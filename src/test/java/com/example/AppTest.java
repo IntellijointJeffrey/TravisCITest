b184dba0a0511b84da7ca3519dcf4c48dbad1f1c
Jeffrey Lee <j.lee@intellijointsurgical.ca>
2021-05-19 16:33:04 -0400

    AddTest2 should Fail

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +17,15 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void addTest1(){
+        assertEquals(2, App.addNums(1,1));
+    }
+
+    @Test
+    public void addTest2(){
+        assertEquals(1, App.addNums(1,1));
     }
 }
